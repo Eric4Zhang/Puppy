eed258ae92812ac0507b5299fa431cab9a2975eb
daoyong.zhang <daoyong.zhang@smith.co>
2019-03-26 19:25:20 -0400

    Updates

---
+++ precache-manifest.4af1b16bca34831c64c06195d5444abb.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "0239c4f6983d78a0ed1b",
+    "url": "/omac-app/static/js/runtime~main.5f93b1a2.js"
+  },
+  {
+    "revision": "39c6b3b5da297a8d6336",
+    "url": "/omac-app/static/js/main.9bb040ef.chunk.js"
+  },
+  {
+    "revision": "1a10c60d9a45401accd5",
+    "url": "/omac-app/static/js/2.1d29e1a4.chunk.js"
+  },
+  {
+    "revision": "39c6b3b5da297a8d6336",
+    "url": "/omac-app/static/css/main.09c12dd2.chunk.css"
+  },
+  {
+    "revision": "b5af73b00928aad80b8d8f7d1501b1ed",
+    "url": "/omac-app/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/omac-app/precache-manifest.7150432335682e681ef5481572cf3308.js"
+  "/omac-app/precache-manifest.4af1b16bca34831c64c06195d5444abb.js"
 );
 
 workbox.clientsClaim();
