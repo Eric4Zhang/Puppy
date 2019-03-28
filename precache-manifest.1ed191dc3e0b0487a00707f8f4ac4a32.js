733850ae29a03b7cafdb222cd681eb5d47261a49
daoyong.zhang <daoyong.zhang@smith.co>
2019-03-28 12:41:58 -0400

    Updates

---
+++ precache-manifest.1ed191dc3e0b0487a00707f8f4ac4a32.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "0239c4f6983d78a0ed1b",
+    "url": "/omac-app/static/js/runtime~main.5f93b1a2.js"
+  },
+  {
+    "revision": "e3aef1a6e934d8de5c57",
+    "url": "/omac-app/static/js/main.c8c5c952.chunk.js"
+  },
+  {
+    "revision": "931dfa292ab5622bed2a",
+    "url": "/omac-app/static/js/2.6b874452.chunk.js"
+  },
+  {
+    "revision": "e3aef1a6e934d8de5c57",
+    "url": "/omac-app/static/css/main.f9b190f1.chunk.css"
+  },
+  {
+    "revision": "9d37cf36c66a2e2eaf3be9c135920a26",
+    "url": "/omac-app/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/omac-app/precache-manifest.0ff62fa03e786224fdfab68fcd508bd3.js"
+  "/omac-app/precache-manifest.1ed191dc3e0b0487a00707f8f4ac4a32.js"
 );
 
 workbox.clientsClaim();
